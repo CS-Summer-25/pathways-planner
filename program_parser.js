93133d5f86306d371d069f9607928d5bc99f154a
Fahad Sultan <syedfahadsultan@gmail.com>
2025-07-08 11:34:37 -0400

    Cleanup + Merging html and js files

--- program_parser.js
+++ program_parser.js
@@ -1,3 +1,6 @@
+// Looks into acalog_programs.json file 
+// loops over programs 
+
 let majors = {};
 let minors = {};
 let GER_COURSES = null;
@@ -197,6 +200,7 @@ async function assignCourses(path) {
     //     }
     // }
 }
+
 async function initialize() {
     // Recall, waiting so that MAJORS assigned properly
     await assignCourses("acalog_programs.json");
@@ -209,8 +213,56 @@ async function initialize() {
     minors = Object.fromEntries(Object.entries(minors).sort());
 
     constructCourses();
+    setGERSAutocomplete();
     console.log("Initialization complete!")
 }
+
+
+function setGERSAutocomplete() {
+    $( function() {        
+        fetch('gers.json')
+        .then(response => response.json())
+        .then(gers => {
+            GER_COURSES = gers;
+            GERS = Object.keys(gers);
+            // Loop over gers dictionary to set up autocomplete for all GER fields
+            for (let gerKey in gers) {
+                if (gers.hasOwnProperty(gerKey)) {
+                    const gerElements = document.getElementsByClassName(gerKey);
+                    for (let i = 0; i < gerElements.length; i++) {
+                        const gerElement = gerElements[i];
+                        $(gerElement).autocomplete({
+                            source: gers[gerKey],
+                            select: function(event, ui) {
+                                inputValue = ui.item.label;
+                                rowLabel = event.target.classList[1];
+                                // firstCol = 
+                                console.log("LABEL: ", rowLabel);
+
+                                if (Object.values(event.target.classList).indexOf("CLPs") == -1) {
+
+                                    // make rowLabel all uppercase 
+                                    rowLabel = rowLabel.toUpperCase();
+                                    var isValidGER = GER_COURSES[rowLabel].indexOf(inputValue) >= 0;
+                        
+                                    if (isValidGER == false){
+                                        event.target.setAttribute("style", "background-color: red;");
+                                    }
+                                    else {
+                                        event.target.setAttribute("style", "background-color: lightgreen;");
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+      } 
+    );
+}
+
 
 // We should assign majors list based on what majors.csv has in the list
 function constructCourses() {
@@ -364,7 +416,7 @@ function updateFirstCol(i, j, table, firstCol) {
 
     var firstCell = relevantRow.cells[0];
     var relevantRowInputs = relevantRow.getElementsByTagName("input");
-    var rowLabel = firstCol[i].toLowerCase();
+    var rowLabel = firstCol[i].toUpperCase();
     if (firstCol[i] != "CLPs") {
         var isValidGER = GER_COURSES[rowLabel].indexOf(inputValue) >= 0;
     }
@@ -512,4 +564,168 @@ function createTable(tableName, tableId, data) {
         }
     }
     addInputRow(tableId, data);
+}
+
+         
+function updateSemesterLabel() {
+    // Should we add something about updating the table when this function is called? Otherwise, data must be re-entered to refresh table
+    var semesterLabel = document.getElementById("semesterLabel");
+    var slider = document.getElementById("semesterSlider");
+    semesterLabel.innerHTML = `${slider.value}`;
+    console.log(`Semester Val is ${slider.value}`);
+
+    updateTableColorsOnSlider(slider.value);
+
+}
+
+function updateTableColorsOnSlider(semester) {
+    // Get all tables GER and Major 
+    var tables = document.querySelectorAll("table");
+
+    // Loop through each table
+    tables.forEach(function(table) {
+        // Get all rows in the table
+        var rows = table.rows;
+        console.log(rows);
+        // Loop through each row, except the header row
+        for (let i = 1; i < rows.length; i++) {
+            var relevantRow = rows[i];
+            var firstCell = relevantRow.cells[0];
+            var relevantRowInputs = relevantRow.getElementsByTagName("input");
+            
+            // Set all inputs to enabled
+            for (let j = 0; j < relevantRowInputs.length; j++) {
+                if(relevantRowInputs[j].value != "" && Object.values(firstCell.classList).indexOf("CLPs") == -1){
+
+                    if (semester-1 == j) {
+                        // If semester is current, set to ongoing
+                        firstCell.setAttribute("style", "background-color: yellow;");
+                    }
+                    else if (semester-1 > j) {
+                        // If semester is past current semester, set to green
+                        firstCell.setAttribute("style", "background-color: green;");
+                    }
+                    else {
+                        // If semester is before current semester, set to planned
+                        firstCell.setAttribute("style", "background-color: blue;");
+                    }
+                }
+
+                if ((j == semester-1) && (relevantRowInputs[j].disabled == false) && (relevantRowInputs[j].value == "")) {
+                    relevantRowInputs[j].setAttribute("style", "background-color: rgb(196, 83, 196);");
+                }
+                else if (relevantRowInputs[j].disabled == false && relevantRowInputs[j].value == "") {
+                    relevantRowInputs[j].setAttribute("style", "background-color: rgb(255, 255, 255, 0.75);");
+                }
+                else if (relevantRowInputs[j].disabled == true && relevantRowInputs[j].value == "") {
+                    relevantRowInputs[j].setAttribute("style", "background-color: rgb(255, 255, 255, 0.25);");
+                }
+            }
+        }
+    });
+}
+
+// Make sure that savePlan and loadPlan save the programs as well
+function savePlan() {
+
+    var currentSemester = document.getElementById("semesterLabel").innerHTML;
+
+    var password = document.getElementById("password").value;
+    console.log(password);
+    var table = document.getElementById("courses");
+    var countGers = GERS.length;
+    var plan = {};
+
+    // Saving plan in compressed form for GET requests 
+    var compressed = "";
+    for (let i = 0; i < countGers; i++){
+        var relevantRow = table.rows[i+1];
+        var inputs = relevantRow.getElementsByTagName("input");
+        var courses = [];
+        for (let j = 0; j < inputs.length; j++) {
+            if (inputs[j].value !== "") {
+                courses.push(inputs[j].value);
+                compressed += `${GERS[i]}_${j+1}_${inputs[j].value};`;
+            }
+        }
+    }
+
+    // Make get request and pass password and plan as query parameters
+    // var xhr = new XMLHttpRequest();
+    plan = "Abc"; // Placeholder for plan, replace with actual plan data
+    var constructedUrl = `https://furmancs.com/tabot/savePlan?password=${encodeURIComponent(password)}&plan=${encodeURIComponent(compressed)}&semester=${currentSemester}`;
+    console.log(constructedUrl);
+    // xhr.open("GET", constructedUrl, true);
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState === 4 && xhr.status === 200) {
+    //         alert("Plan saved successfully!");
+    //     } else if (xhr.readyState === 4) {
+    //         alert("Failed to save plan. Please try again.");
+    //     }
+    //     else{
+    //         console.log("Request in progress...");
+    //     }
+    // };
+
+    fetch(constructedUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    });
+
+}
+
+function loadPlan() {
+
+    var password = document.getElementById("password").value;
+
+    console.log(password);
+    console.log("Load Plan");
+    var constructedUrl = `https://furmancs.com/tabot/loadPlan?password=${encodeURIComponent(password)}`;
+
+    fetch(constructedUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        var coursesInfo = response.json();
+
+        coursesInfo.then(data => {
+
+            for(i = 0; i<data.length; i++){
+                console.log(data[i]);
+                let course = data[i];
+                let ger = course.ger;
+                let semester = parseInt(course.sem);
+                let courseName = course.title;
+
+                console.log(ger);
+
+                // Find the row corresponding to the GER
+                let gerIndex = GERS.indexOf(ger);
+                console.log(gerIndex);
+                let table = document.getElementById("courses");
+                let relevantRow = table.rows[gerIndex + 1];
+                let inputs = relevantRow.getElementsByTagName("input");
+                console.log(semester);
+                // Find the input corresponding to the semester
+                if (semester >= 1 && semester <= 8) {
+                    let inputIndex = semester - 1; // Adjust for zero-based index
+                    console.log(inputs[inputIndex]);
+                    if (inputs[inputIndex]) {
+                        inputs[inputIndex].value = courseName;
+                        inputs[inputIndex].dispatchEvent(new Event('input')); // Trigger input event to update styles
+                    }
+                }
+            }
+            
+        }).catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
+            alert("Failed to load plan. Please check your password and try again.");
+
+        
+        });
+    });
 }
